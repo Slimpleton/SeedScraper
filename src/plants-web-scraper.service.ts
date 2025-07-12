df9ad00b18ca636df4ee5935d35d1978bbbd66ff
Danny Gione <thegiones13@sbcglobal.net>
2025-07-12 14:23:02 -0700

    Properly pulling and writing the common names of the plants

--- src/plants-web-scraper.service.ts
+++ src/plants-web-scraper.service.ts
@@ -1,43 +1,45 @@
-import puppeteer, { Browser, Page } from 'puppeteer';
-import { catchError, concatAll, debounceTime, defer, delay, forkJoin, from, interval, map, mergeAll, Observable, of, shareReplay, Subject, switchMap, takeUntil, tap, withLatestFrom, zip } from 'rxjs';
+import puppeteer, { Browser } from 'puppeteer';
+import { catchError, defer, forkJoin, from, interval, Observable, of, shareReplay, Subject, switchMap, takeUntil, tap, zip } from 'rxjs';
 import { GovPlantsDataService } from './PLANTS_data.service';
 import fs from 'fs';
 
 export class PlantsWebScraperService {
+  public readonly usdaGovPlantProfileUrl: string = 'https://plants.usda.gov/plant-profile/';
+
   private readonly _CSVName: string = 'PLANTS_EXTRA_DATA.csv';
   private readonly _CSVHeaders: string[] = ['Accepted Symbol', 'Counties', 'Common Name'];
   private readonly _PlantProfileHeaderName: string = 'plant-profile-header';
-  public readonly usdaGovPlantProfileUrl: string = 'https://plants.usda.gov/plant-profile/';
-
+  private readonly _csvPath = './assets/' + this._CSVName;
   private readonly _ngDestroy$: Subject<void> = new Subject<void>();
-  // TODO this is not accepted it breaks for some reason?? not changing to observable correctly prob
+  private readonly _csvWriter$: Subject<string> = new Subject<string>();
   private readonly _browserRequest$: Observable<Browser> = from(puppeteer.launch({
     headless: false,
-    slowMo: 250,
+    slowMo: 100,
     executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
   })).pipe(
     shareReplay(1),
     takeUntil(this._ngDestroy$));
 
-  // public get newPage$(): Observable<Page> {
-  //   return this._browserRequest$.pipe(
-  //     switchMap((browser: Browser) => from(browser.newPage())),
-  //   );
-  // }
-
-  constructor(private readonly _plantDataService: GovPlantsDataService) { }
+  constructor(private readonly _plantDataService: GovPlantsDataService) {
+    this._csvWriter$.pipe(
+      tap((value) => {
+        console.log('Writing Value ' + value);
+        fs.appendFileSync(this._csvPath, value);
+        fs.appendFileSync(this._csvPath, "\r\n");
+      }))
+      .subscribe();
+  }
 
   public write(): Observable<any> {
-    const csvPath = './assets/' + this._CSVName;
-    const escapedHeaders = this._CSVHeaders.map((x: string) => '"' + x + '"').join(',');
+    const escapedHeaders = this._CSVHeaders.map((x: string) => '"' + x + '"').join(',') + '\r\n';
     const allIdsCSVPath = './assets/allIds.csv';
 
     return this._plantDataService.getAllNativePlantIds().pipe(
       switchMap((ids: ReadonlyArray<string>) => {
         // If file does not exist, create the file with headers
-        if (fs.existsSync(csvPath))
-          fs.unlinkSync(csvPath);
-        fs.writeFileSync(csvPath, escapedHeaders);
+        if (fs.existsSync(this._csvPath))
+          fs.unlinkSync(this._csvPath);
+        fs.writeFileSync(this._csvPath, escapedHeaders);
 
         if (fs.existsSync(allIdsCSVPath))
           fs.unlinkSync(allIdsCSVPath);
@@ -61,26 +63,31 @@ export class PlantsWebScraperService {
   }
 
   private async writeSpecies(browser: Browser, id: string) {
-    const csvPath = './assets/' + this._CSVName;
-
     const page = await browser.newPage();
     await page.goto(`${this.usdaGovPlantProfileUrl}${id}`);
-    await page.evaluate((selector: string) => {
-      try {
-        const childrenElements: HTMLCollection | undefined = document.querySelector(selector)?.children;
-        if (childrenElements) {
-          for (let i = 1; i < childrenElements.length; i++) {
-            const childElement: Element = childrenElements.item(i)!;
-            if (childElement.tagName === 'H2' && childElement.textContent && childElement.textContent.length > 0) {
-              fs.appendFileSync(csvPath, `${id},${'null'},${childElement.textContent!}`);
-            }
+    const parentElement = await page.waitForSelector(this._PlantProfileHeaderName);
+    if (parentElement) {
+      const csvRow = await page.evaluate((parentEl: Element, plantId: string) => {
+        const childrenElements = parentEl.children;
+
+        for (let i = 1; i < childrenElements.length; i++) {
+          const childElement = childrenElements.item(i);
+
+          if (childElement &&
+            childElement.tagName === 'H2' &&
+            childElement.textContent?.trim()) {
+
+            const escapedText = childElement.textContent.trim().replace(/"/g, '""');
+            return `"${plantId}","null","${escapedText}"`;
           }
         }
-      } catch (error: any) {
-        console.error(error);
-      }
-      // It will never be the first item so skippity dippity
-    }, this._PlantProfileHeaderName);
+
+        return `"${plantId}","",""`;
+      }, parentElement, id);
+
+      this._csvWriter$.next(csvRow);
+      console.log(csvRow);
+    }
 
     await page.close();
   }
@@ -90,7 +97,7 @@ export class PlantsWebScraperService {
     return defer(() => (this.writeSpecies(browser, id)));
   }
 
-  destroy(): void {
+  public destroy(): void {
     this._ngDestroy$.next();
     this._ngDestroy$.complete();
   }
