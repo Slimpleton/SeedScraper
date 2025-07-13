ec88705a9783c617490b437da693d21faa43b696
Slimpleton <daniel.gione@gmail.com>
2025-07-13 11:12:53 -0700

    trying to make proaress on the modal. got it to open and the download to start but not collecitng it / closing properly

--- src/plants-web-scraper.service.ts
+++ src/plants-web-scraper.service.ts
@@ -1,11 +1,12 @@
-import puppeteer, { Browser } from 'puppeteer';
-import { catchError, defer, forkJoin, from, interval, Observable, of, shareReplay, Subject, switchMap, takeUntil, tap, zip } from 'rxjs';
+import puppeteer, { Browser, Page } from 'puppeteer';
+import { catchError, defer, first, forkJoin, from, interval, Observable, of, shareReplay, Subject, switchMap, takeUntil, tap, zip } from 'rxjs';
 import { GovPlantsDataService } from './PLANTS_data.service';
 import fs from 'fs';
 
 export class PlantsWebScraperService {
   public readonly usdaGovPlantProfileUrl: string = 'https://plants.usda.gov/plant-profile/';
 
+  private readonly _NEW_PAGE_INTERVAL_MS = 2000;
   private readonly _CSVName: string = 'PLANTS_EXTRA_DATA.csv';
   private readonly _CSVHeaders: string[] = ['Accepted Symbol', 'Counties', 'Common Name'];
   private readonly _PlantProfileHeaderName: string = 'plant-profile-header';
@@ -13,12 +14,12 @@ export class PlantsWebScraperService {
   private readonly _ngDestroy$: Subject<void> = new Subject<void>();
   private readonly _csvWriter$: Subject<string> = new Subject<string>();
   private readonly _browserRequest$: Observable<Browser> = from(puppeteer.launch({
-    headless: true,
-    slowMo: 50,
+    headless: false,
     executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
   })).pipe(
     shareReplay(1),
     takeUntil(this._ngDestroy$));
+
 
   constructor(private readonly _plantDataService: GovPlantsDataService) {
     this._csvWriter$.pipe(
@@ -50,7 +51,7 @@ export class PlantsWebScraperService {
       switchMap(([ids, browser]: [ReadonlyArray<string>, Browser]) =>
         zip(
           from(ids),
-          interval(500)
+          interval(this._NEW_PAGE_INTERVAL_MS)
         ).pipe(
           switchMap(([id, _]) => this.writeSpeciesRxjs(browser, id)))
       ),
@@ -65,9 +66,38 @@ export class PlantsWebScraperService {
   private async writeSpecies(browser: Browser, id: string) {
     const page = await browser.newPage();
     await page.goto(`${this.usdaGovPlantProfileUrl}${id}`);
+
+    const downloadLinkClass = '.download-distribution-link';
+    const linkElement = await page.waitForSelector(downloadLinkClass);
+    await linkElement?.click();
+
+    const downloadButton = await page.waitForSelector('a[download]');
+    if (downloadButton) {
+      const newTabUrl: string = await page.evaluate((downloadButton: Element) => {
+        const tabUrl: string | null = downloadButton.getAttribute('download');
+        if (tabUrl == null)
+          throw new Error('null download');
+
+        return tabUrl;
+      }, downloadButton);
+
+      // TODO contains the name of the csv for some reason god this is hard
+      console.log(newTabUrl);
+
+      await downloadButton.click();
+      const popup = await browser.waitForTarget((target) => target.url().includes(newTabUrl));
+      const popupPage = await popup.asPage();
+      popupPage.setRequestInterception(true);
+      const json = popupPage.on('request', async (request) => { 
+          return await request.response()?.text();
+       });
+
+       console.log(json);
+    }
+
     const parentElement = await page.waitForSelector(this._PlantProfileHeaderName);
     if (parentElement) {
-      const csvRow = await page.evaluate((parentEl: Element, plantId: string) => {
+      const commonName = await page.evaluate((parentEl: Element) => {
         const childrenElements = parentEl.children;
 
         for (let i = 1; i < childrenElements.length; i++) {
@@ -78,13 +108,14 @@ export class PlantsWebScraperService {
             childElement.textContent?.trim()) {
 
             const escapedText = childElement.textContent.trim().replace(/"/g, '""');
-            return `"${plantId}","null","${escapedText}"`;
+            return escapedText;
           }
         }
 
-        return `"${plantId}","",""`;
-      }, parentElement, id);
+        return "";
+      }, parentElement);
 
+      const csvRow: string = `"${id}","${null}","${commonName}"`;
       this._csvWriter$.next(csvRow);
       console.log(csvRow);
     }
